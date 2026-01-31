31a322d46793675390d7493fa1394468bda64cb7
kavimaran2012-byte <kavimaran2012@gmail.com>
2026-01-31 07:27:02 -0500

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number: "))
+if (i< 15):
+    print("i is smaller than 15")
+    print("I am in if block")
+else:
+    print("I is greater than 15")
+    print("I am in else block")
+print("I am not in if and not is else block")
